a5ee637c671aa1621269c4cf46a66d828f44f00d
Vadim <119fps.com>
2021-10-23 20:56:38 +0300

    logic done. UI broked

--- KontushVI_2.2/ViewController.swift
+++ KontushVI_2.2/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var mainViewOutlet: UIView!
+    @IBOutlet var redSliderOutlet: UISlider!
+    @IBOutlet var greenSliderOutlet: UISlider!
+    @IBOutlet var blueSliderOutlet: UISlider!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLable: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redSliderOutlet.minimumTrackTintColor = .red
+        greenSliderOutlet.minimumTrackTintColor = .green
+        blueSliderOutlet.minimumTrackTintColor = .blue
+        mainViewOutlet.backgroundColor = .black
     }
 
-
+    @IBAction func redSliderAction() {
+        redLabel.text = String(format:"%.2f", redSliderOutlet.value)
+        
+        mainViewOutlet.backgroundColor = UIColor(red: CGFloat(redSliderOutlet.value), green: CGFloat(greenSliderOutlet.value), blue: CGFloat(blueSliderOutlet.value), alpha: 1)
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenLable.text = String(format:"%.2f", greenSliderOutlet.value)
+        
+        mainViewOutlet.backgroundColor = UIColor(red: CGFloat(redSliderOutlet.value), green: CGFloat(greenSliderOutlet.value), blue: CGFloat(blueSliderOutlet.value), alpha: 1)
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueLabel.text = String(format:"%.2f", blueSliderOutlet.value)
+        
+        mainViewOutlet.backgroundColor = UIColor(red: CGFloat(redSliderOutlet.value), green: CGFloat(greenSliderOutlet.value), blue: CGFloat(blueSliderOutlet.value), alpha: 1)
+    }
 }
 
