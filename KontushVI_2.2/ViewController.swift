88034082e41365e02e1498a619180949d19716a7
Vadim <119fps.com>
2021-10-23 15:46:21 +0300

    Initial Commit

---
+++ KontushVI_2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KontushVI_2.2
+//
+//  Created by Vadim Kontush on 23.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
